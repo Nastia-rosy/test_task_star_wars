afc447743a3d1c30cbb2bbc911633a7a32115a8f
Nastia-rosy <nastiiarosy@gmail.com>
2020-08-09 12:33:50 +0300

    Updates

---
+++ precache-manifest.5060e6ce73cf8a162747d3e061a94beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128be9038f9577e0524dd6d945095671",
+    "url": "/Nastia-rosy/test_task_star_wars/index.html"
+  },
+  {
+    "revision": "8d3c0adc85b59dfca30f",
+    "url": "/Nastia-rosy/test_task_star_wars/static/css/main.2ce75fa4.chunk.css"
+  },
+  {
+    "revision": "3d2bfb5d7c74b6e4e385",
+    "url": "/Nastia-rosy/test_task_star_wars/static/js/2.11e433e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nastia-rosy/test_task_star_wars/static/js/2.11e433e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3c0adc85b59dfca30f",
+    "url": "/Nastia-rosy/test_task_star_wars/static/js/main.03952043.chunk.js"
+  },
+  {
+    "revision": "80abc070566a90d05a50",
+    "url": "/Nastia-rosy/test_task_star_wars/static/js/runtime-main.61a6e328.js"
+  }
+]);
